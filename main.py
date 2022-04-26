2a7b38f69f9b2f29f51a96bf2fdc61c50e7b7336
Lunk Will <tjthejuggler@gmail.com>
2022-04-26 16:51:47 +0300

    new promgen filter pretty much working

--- main.py
+++ main.py
@@ -6,43 +6,42 @@ from os.path import exists #file locations and directories
 from profanity_filter import ProfanityFilter #used censoring
 from PIL import Image #show images
 
-pf = ProfanityFilter()  #set up our sensor(to maybe be used later)
-censored = False
-directory = './directories' #tells us where our images and settings(jsons) files are
-json_objects = [] #empty list
-
-for subdir, dirs, files in os.walk(directory): #looping through every folder and every file in ./directories
-	for filename in files: #we do the following things to every file it finds
-		this_path_to_the_file = os.path.join(subdir, filename) # set the file path to a text variable
-		settings_file_name = filename.split('_')[0] + "_settings.txt"
-		if filename.endswith(".png") and exists(this_path_to_the_file.split(filename)[0]+settings_file_name):	#check to see if this file in an image(png) and if it has a settings txt file		
-			#if both these conditions are met, then we continue, if not we move on to the next file
-			with open(this_path_to_the_file.split(filename)[0]+settings_file_name, 'r') as f: #we open the text(settings/json) file
-				use_this_image = True #we are going to add this image to our app
-				data = json.load(f) #gets the text from the settings file
-				text = f.read()
-				if type(data.get('text_prompts')) is dict:
-					data['text_prompts'] = str(data['text_prompts']["0"])
-				else:
-					data['text_prompts'] = str(data['text_prompts'])
-				json_object = {
-					"path": this_path_to_the_file,
-					"text": data
-				}
-				for item in data['text_prompts']:
-					if censored and pf.is_profane(item):
-						print(item)
-						use_this_image = False
-				if use_this_image: #if we are using this image, then we add its information to our json_objects list
-					json_objects.append(json_object)
-
-print("Count:", len(json_objects)) #show us how many images we are able to use
+def build_image_catalog():
+	pf = ProfanityFilter()  #set up our sensor(to maybe be used later)
+	censored = False
+	directory = './directories' #tells us where our images and settings(jsons) files are
+	image_data_objects = [] #empty list
+	for subdir, dirs, files in os.walk(directory): #looping through every folder and every file in ./directories
+		for filename in files: #we do the following things to every file it finds
+			this_path_to_the_file = os.path.join(subdir, filename) # set the file path to a text variable
+			settings_file_name = filename.split('_')[0] + "_settings.txt"
+			if filename.endswith(".png") and exists(this_path_to_the_file.split(filename)[0]+settings_file_name):	#check to see if this file in an image(png) and if it has a settings txt file		
+				#if both these conditions are met, then we continue, if not we move on to the next file
+				with open(this_path_to_the_file.split(filename)[0]+settings_file_name, 'r') as f: #we open the text(settings/json) file
+					use_this_image = True #we are going to add this image to our app
+					data = json.load(f) #gets the text from the settings file
+					text = f.read()
+					if type(data.get('text_prompts')) is dict:
+						data['text_prompts'] = str(data['text_prompts']["0"])
+					else:
+						data['text_prompts'] = str(data['text_prompts'])
+					json_object = {
+						"path": this_path_to_the_file,
+						"text": data
+					}
+					for item in data['text_prompts']:
+						if censored and pf.is_profane(item):
+							print(item)
+							use_this_image = False
+					if use_this_image: #if we are using this image, then we add its information to our image_data_objects list
+						image_data_objects.append(json_object)
+	return image_data_objects
 
 #this function gets created now, but it will be used later
-def get_filtered_images(user_input): #goes through all the text files that match the images that we are going to use
+def get_filtered_images(user_input, image_data_objects): #goes through all the text files that match the images that we are going to use
 	images_prompt_list = []
 	images_path_list = []
-	for data in json_objects: #we loop through each text file
+	for data in image_data_objects: #we loop through each text file
 		#print('1', data['text'])
 		if user_input.lower() in data['text']['text_prompts'].lower():
 			result = json.dumps(data['text']['text_prompts']) # we look at the prompts
@@ -53,13 +52,7 @@ def get_filtered_images(user_input): #goes through all the text files that match
 
 #design our GUI(graphical user interface)
 class ExampleApp(tk.Tk):
-	def __init__(self):
-		self.images_path_list = []   #we make two empty lists, that we will fill later
-		self.image_prompts_list = []
-		tk.Tk.__init__(self) #required thing for making a tkinter GUI
-		langs_var = tk.StringVar(value=())
-		self.images_listbox = tk.Listbox(self, listvariable=langs_var, width=400, height=10, selectmode='extended')
-		self.images_listbox.pack(fill="both", expand=True)
+	def __init__(self, image_data_objects):
 		def image_chosen_by_user(*args):
 			index_of_selected_item = self.images_listbox.curselection()[0]
 			path_of_currently_selected_item = self.images_path_list[index_of_selected_item]
@@ -69,28 +62,14 @@ class ExampleApp(tk.Tk):
 			self.data.delete('1.0', tk.END)
 			self.data.insert("end", "test")
 			self.data.configure(state="disabled")
-		self.images_listbox.bind("<Return>", image_chosen_by_user)
-		self.images_listbox.bind('<Double-Button>', image_chosen_by_user)
 
-		self.user_text = tk.StringVar() # used with the tkinter entry object
-		self.images_path_list, self.image_prompts_list = get_filtered_images(self.user_text.get())
-		for ind,prompt in enumerate(self.image_prompts_list):
-			self.ending = '('+self.images_path_list[ind].split('(')[1]
-			self.images_listbox.insert("end", prompt + self.ending+'\n')
-
-		def user_search_entry_changed(*args): #filters our image list based on the users entry
+		def update_main_prompt_list(*args): #filters our image list based on the users entry
 			self.images_listbox.delete(0, tk.END)
-			print('self.user_text.get()', self.user_text.get())
-			self.images_path_list, self.image_prompts_list = get_filtered_images(self.user_text.get())
+			self.images_path_list, self.image_prompts_list = get_filtered_images(self.users_search_text.get(), image_data_objects)
 			for ind,prompt in enumerate(self.image_prompts_list):	
 				self.ending = '('+self.images_path_list[ind].split('(')[1]
 				self.images_listbox.insert("end", prompt + self.ending+'\n')
-			return True		
-
-		self.user_text.trace_add('write', user_search_entry_changed) #'user_search_entry_changed' gets run whenever the user_text entry changes 
-		self.user_entry = tk.Entry(self, bd =5, textvariable=self.user_text) #actually create our entry
-		self.user_entry.pack(side="bottom", fill="x") #add our entry to our tkinter window
-		self.data_window= tk.Toplevel(self) #require tkinter line
+			return True	
 
 		def click_data_reset(*args):
 			print('click_data_reset')
@@ -108,21 +87,47 @@ class ExampleApp(tk.Tk):
 		def click_postdata_save(*args):	
 			print('click_postdata_save')
 
+		self.images_path_list = []   
+		self.image_prompts_list = []
+		tk.Tk.__init__(self) #required thing for making a tkinter GUI
+
+		#widget that shows prompt/image list
+		image_prompts_list_variable = tk.StringVar(value=())
+		self.images_listbox = tk.Listbox(self, listvariable=image_prompts_list_variable, width=400, height=10, selectmode='extended')
+		self.images_listbox.pack(fill="both", expand=True)
+		self.images_listbox.bind("<Return>", image_chosen_by_user)
+		self.images_listbox.bind('<Double-Button>', image_chosen_by_user)
+
+		self.users_search_text = tk.StringVar()
+		self.users_search_text.trace_add('write', update_main_prompt_list) #'update_main_prompt_list' gets run whenever the users_search_text entry changes 
+		self.user_entry = tk.Entry(self, bd =5, textvariable=self.users_search_text) #actually create our entry
+		self.user_entry.pack(side="bottom", fill="x") #add our entry to our tkinter window
+		
+		self.data_window= tk.Toplevel(self) #require tkinter line
 		self.data = tk.Text(self.data_window, wrap="word", width=400, height=25, cursor="xterm #0000FF")
 		self.data.pack(side="top", fill="x")
+		
 		self.data_reset = tk.Button(self.data_window, text="data reset", command=click_data_reset)#maybe getting rid of self. fixed this, test print
 		self.data_reset.pack(side="top", fill="x")
+		
 		self.data_save = tk.Button(self.data_window, text="data save", command=click_data_save)
 		self.data_save.pack(side="top", fill="x")
+		
 		self.postdata = tk.Text(self.data_window, wrap="word", width=400, height=25, cursor="xterm #0000FF")
 		self.postdata.pack(side="top", fill="x")
+		
 		self.postdata_reset = tk.Button(self.data_window, text="postdata reset", command=click_postdata_reset)
 		self.postdata_reset.pack(side="top", fill="x")
+		
 		self.postdata_save = tk.Button(self.data_window, text="postdata save", command=click_postdata_save)
 		self.postdata_save.pack(side="top", fill="x")
 
+		update_main_prompt_list()
+
 if __name__ == "__main__":
-	app = ExampleApp()
+	image_data_objects = build_image_catalog()
+	print("Count:", len(image_data_objects)) #show us how many images we are able to use
+	app = ExampleApp(image_data_objects)
 	app.mainloop()
 
 # #this needs worked on
--- promgen.py
+++ promgen.py
@@ -5,6 +5,7 @@ from os import path
 import argparse
 import re
 import os
+import textwrap
 
 api_key = os.environ.get("OPEN_AI_API")
 openai.api_key = api_key 
@@ -12,30 +13,58 @@ openai.api_key = api_key
 engine = "ada"
 engine = "davinci"
 
-styles_file = open( "prompgen_styles.txt", "r")
-styles = styles_file.readlines()
-styles_file.close()
-artists_file = open( "prompgen_artists.txt", "r")
-artists = artists_file.readlines()
-artists_file.close()
-keywords_file = open( "prompgen_keywords.txt", "r")
-keywords = keywords_file.readlines()
-keywords_file.close()
-prompts_file = open( "prompgen_prompts.txt", "r")
-pre_prompts = prompts_file.readlines()
-prompts_file.close()
-artist_intros = ["in the style of","by","inspired by","resembling"]
+category_keys = {'2': 'religious', '3': 'hyperrealistic'}
+
+def load_modifiers():
+	styles_file = open( "prompgen_styles.txt", "r")
+	styles = styles_file.readlines()
+	styles_file.close()
+	artists_file = open( "prompgen_artists.txt", "r")
+	artists = artists_file.readlines()
+	artists_file.close()
+
+	artists_dict = {'bob' : ["religious", "hyperrealistic"], "charlie" : ["hyperrealistic", "happy"]}
+	# if path.exists(cwd+'/promgen_artists_formatted.txt'):			
+	# 	with open(cwd+'/promgen_artists_formatted.txt') as json_file:
+	# 		artists_dict = json.load(json_file)
+
+	keywords_file = open( "prompgen_keywords.txt", "r")
+	keywords = keywords_file.readlines()
+	keywords_file.close()
+	prompts_file = open( "prompgen_prompts.txt", "r")
+	pre_prompts = prompts_file.readlines()
+	prompts_file.close()
+	artist_intros = ["in the style of","by","inspired by","resembling"]
+
+	return (styles, artists_dict, keywords, pre_prompts, artist_intros)
 
 def get_args():
 	user_input, batch_size = 'a boy', 25
-	parser = argparse.ArgumentParser()
+	every_categories_filter, only_categories_filter = [], []
+	parser = argparse.ArgumentParser(	    
+		prog='PROG',
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		description=textwrap.dedent('''\
+			for use with -e and -a, comma seperated (ex: -e 1,5,14)
+			Category Key:
+			2 - religious
+			3 - hyperrealistic
+
+			'''))
 	parser.add_argument("prompt", help="the base prompt (comma seperate each weighted section")
 	parser.add_argument("-b", "--batchsize", type = int, help="batch_size, the number of images")
+	parser.add_argument("-e", "--everycat", type = str, help="use every modifier in these categories")
+	parser.add_argument("-o", "--onlycat", type = str, help="use only modifiers that have all these categories")
+
 	args = parser.parse_args()
 	if args.batchsize:
 		batch_size = args.batchsize	
+	if args.everycat:
+		every_categories_filter = [x for x in args.everycat.split(",")]
+	if args.onlycat:
+		only_categories_filter = [x for x in args.onlycat.split(",")]
 	user_input = args.prompt
-	return (user_input, batch_size)
+	return (user_input, batch_size, every_categories_filter, only_categories_filter)
 
 def rand_item(my_list):
 	intro = ''
@@ -47,7 +76,7 @@ def rand_w():
 	to_ret = str(random.randint(1,9))
 	return (to_ret)
 
-def get_result(user_prompt, pre_prompts):
+def get_gpt_result(user_prompt, pre_prompts):
 	prompt = """
 	* """+ random.choice(pre_prompts) + """
 	* """+ random.choice(pre_prompts) + """
@@ -68,8 +97,44 @@ def create_output_file(filename, output_lines):
 		for item in output_lines:
 			f.write("[%s]\n" % item.strip("\n"))
 
+def get_every_filter(artists_dict, filter_list):
+	listOfKeys = list()
+	listOfItems = artists_dict.items()
+	for filter_key in filter_list:
+		filter_word = category_keys[filter_key]
+		for artist in artists_dict.keys():
+			if filter_word in artists_dict[artist] and artist not in listOfKeys:
+				listOfKeys.append(artist)
+	return listOfKeys
+
+def get_only_filter(artists_dict, filter_list):
+	listOfKeys = list()
+	listOfItems = artists_dict.items()
+	for artist in artists_dict.keys():
+		should_append = True
+		for filter_key in filter_list: 
+			filter_word = category_keys[filter_key]
+			if not (filter_word in artists_dict[artist] and artist not in listOfKeys):
+				should_append = False
+		if should_append:
+			listOfKeys.append(artist)
+	return listOfKeys
+
 def main():
-	(user_input, batch_size) = get_args()
+	user_input, batch_size, every_categories_filter, only_categories_filter = get_args()
+	styles_file = open( "prompgen_styles.txt", "r")
+	styles, artists_dict, keywords, pre_prompts, artist_intros = load_modifiers()
+	artist_intros = ["in the style of","by","inspired by","resembling"]
+	filtered_artists = artists_dict.keys()
+	if every_categories_filter and only_categories_filter:
+		print("You can't use 'every' filter and 'only' filter together")
+		quit()
+	elif every_categories_filter:
+		filtered_artists = get_every_filter(artists_dict, every_categories_filter)
+		print(filtered_artists)
+	elif only_categories_filter:
+		filtered_artists = get_only_filter(artists_dict, only_categories_filter)
+		print(filtered_artists)
 	user_prompt = ''	
 	prompts = []
 	print('user_input', user_input)
@@ -84,7 +149,7 @@ def main():
 			if section[0] == "$": #style is used
 				prompt_to_append = prompt_to_append + rand_item(styles)
 			elif section[0] == "@": #artist is used
-				prompt_to_append = prompt_to_append + rand_item(artists)
+				prompt_to_append = prompt_to_append + rand_item(filtered_artists)
 			elif section[0] == "^": #keyword is used
 				prompt_to_append = prompt_to_append + rand_item(keywords)
 			elif section[0] == ":":
@@ -93,7 +158,7 @@ def main():
 			else:
 				if ">" in section and section[0] != ">":
 					user_prompt = section.split(">")[0]
-					result = get_result(user_prompt, pre_prompts)
+					result = get_gpt_result(user_prompt, pre_prompts)
 					prompt_to_append = prompt_to_append + user_prompt+' '+result
 					if section[-1] == ":":
 						prompt_to_append = prompt_to_append + ":"+rand_w()
