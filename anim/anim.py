60d83f9f932bff7e477a06d82207751f43fe6377
Lunk Will <tjthejuggler@gmail.com>
2022-04-27 18:42:35 +0300

    anim formatter script

--- anim/anim.py
+++ anim/anim.py
@@ -1,15 +1,22 @@
-#this program takes the text from each line in source.txt and prints TO TERMINAL in the format shown in result.txt
+#this program takes the text from each line in source.txt and prints TO TERMINAL(print) in the format shown in result.txt
 
-import argparse
+import argparse #this is library that allows arguments to be passed in (python3 anim.py ARGUMENT(24))
 
+
+
+#code for passing arguments when the program is run
 parser = argparse.ArgumentParser()
-parser.add_argument("frames", help="the number of frames between each prompt")
+parser.add_argument("frames", help="the  number of frames between each prompt")
 args = parser.parse_args()
-print("THE FRAMES AGRUMENT PASSED BY THE USER IS: " + args.frames +"\n")
 
-print("THE LINES IN SOURCE.TXT ARE: ")
 file = open( "source.txt", "r")
 lines = file.readlines()
+
+time = 0
+
 for line in lines:
-	print(line.strip())
+	print('"'+ str(time) + '":[ " ' + line.strip() +' " ]')
+	time=time+int(args.frames)
+
+
 
